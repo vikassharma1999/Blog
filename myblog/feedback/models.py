a287714a242efe9eef85dce37f88c8e55d015bf7
vikas sharma <vikas61999@example.com>
2018-07-19 18:34:35 +0530

    add textfield in feedback

--- myblog/feedback/models.py
+++ myblog/feedback/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class FeedBack(models.Model):
-	feed_back=models.CharField(max_length=200)
+	feed_back=models.TextField(max_length=200)
 
 	def __str__(self):
 		return self.feed_back
